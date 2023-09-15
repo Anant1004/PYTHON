19561e26db6293a3a5bd092e3e178d5e2affff13
Anant1004 <anant1042005@gmail.com>
2023-09-15 11:40:25 +0530

    commit

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,21 +1,5 @@
-# a = 1
-# b = 2 
-# print(a+b)
-# print("HI , m name is anant")
-# print("ram",21, "bad_boy",sep="--",end="@")
-# print("hi i am anant")
-# num = int(input("Enter the number"))
-# print(num)
-print("hi i am anant kumar")
-num = int(input("Enter the number"))
-
-def chek_num ():
-    if(num>0):
-        print("number is positive")
-    elif(num<0):
-        print("number is negative")
-    else:
-        print("number is zero")
-    
-chek_num()
-        
+a = int(input())
+b = int(input())
+print(a+b)
+print(type(a))
+list1 = [1,2.5,"appple","2+6i"]
---
+++ user_input.py
@@ -0,0 +1,7 @@
+# a = int(input())
+# b = int(input())
+# print(a+b)
+# print(type(a))
+list1 = [1,2.5,'appple','2+6i']
+print(list1)
+print(type(list1))

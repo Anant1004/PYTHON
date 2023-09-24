0405c8920651496c73d4173ae2e87b5a07c7e9af
Anant1004 <anant1042005@gmail.com>
2023-09-24 22:01:24 +0530

    Encryption_messaging_program

--- Encryption_messaging_program.py
+++ Encryption_messaging_program.py
@@ -1,25 +1,47 @@
 import random
 import string
 
-word = "anant"
-
 def Encrypt_message (word):
     random_chars = ''.join(random.choice(string.ascii_letters)for _ in range(3))
     word =  random_chars + word[1:] + word[0] + random_chars
     return word
 
-def reverse(word):
+def reverse_encrypt(word):
     return word[1:] + word[0]
 
 
 def code (word):
     if(len(word)>= 3):
-         return Encrypt_message(word)
+        return Encrypt_message(word)
     else:
-         return reverse(word)
+        return reverse_encrypt(word)
 
-input_from_user =input("Enter the secret message for ( ENCRYPTION ) : ")
-Words_process = input_from_user.split()
-Encoded_words = [code(word) for word in Words_process]
-Encoded_sentece = ' '.join(Encoded_words)
-print(f"your encoded statement is : {Encoded_sentece}")
+
+def Decrypt_message(word):
+    decrypted_word = word[3:-3] 
+    last_letter = word[-1] + decrypted_word
+    return last_letter
+
+def reverse_decrypt(word):
+    return word[::-1]
+
+def decode(word):
+    if len(word) < 3:
+        return reverse_decrypt(word)
+    else:
+        return Decrypt_message(word)
+
+msg = int(input("Enter 0 for ENCRYPTION //// Enter 1 for DECRYPTION \n"))
+if (msg == 0):
+    input_from_user = input("Enter the secret message for ( ENCRYPTION ) : ")
+    Words_process = input_from_user.split()
+    Encoded_words = [code(word) for word in Words_process]
+    Encoded_sentece = ' '.join(Encoded_words)
+    print(f"ENCODED STATEMENT IS --->: {Encoded_sentece}")
+if(msg == 1):
+    input_from_user = input("Enter the secret message for ( DECRYPTION ) : ")
+    Words_process = input_from_user.split()
+    Decoded_words = [decode(word) for word in Words_process]
+    Decoded_sentence = ' '.join(Decoded_words)
+    print(f"Your decoded statement is: {Decoded_sentence}")
+
